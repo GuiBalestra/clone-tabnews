fb74fb10baa279cca41650e656a943de1dbb958f
Guilherme Taveira Balestra de Oliveira <39175382+GuiBalestra@users.noreply.github.com>
2024-07-23 18:31:20 +0000

    adicionado em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
+import img from "../public/construcao.png";
+import Image from "next/image";
+
 function Home() {
-  return <h1>Estou aprendendo coisas novas!</h1>;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        boxSizing: "border-box",
+        padding: "0",
+        margin: "0",
+        backgroundColor: "#CCC",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image src={img} alt="Em construção" width={1080} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
