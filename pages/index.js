f8b2cbb346de7ed0e13b9fe1d4706ef31ddc1f89
Guilherme Taveira Balestra de Oliveira <39175382+GuiBalestra@users.noreply.github.com>
2024-07-24 23:11:56 +0000

    remove imagem

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,5 @@
-import img from "../public/construcao.png";
-import Image from "next/image";
-
 function Home() {
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        boxSizing: "border-box",
-        padding: "0",
-        margin: "0",
-        backgroundColor: "#CCC",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image src={img} alt="Em construção" width={1080} />
-      </div>
-    </div>
-  );
+  return <div>Em construção</div>;
 }
 
 export default Home;
