d2f9757844953cae72fe173285346fed6d57c1df
Guilherme Taveira Balestra de Oliveira <39175382+GuiBalestra@users.noreply.github.com>
2024-09-03 16:41:13 +0000

    adds  script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+const checkPostgres = () => {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+};
+
+const handleReturn = (error, stdout) => {
+  if (stdout.search("accepting connections") === -1) {
+    process.stdout.write(".");
+    checkPostgres();
+    return;
+  }
+
+  console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
+};
+
+process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
+checkPostgres();
